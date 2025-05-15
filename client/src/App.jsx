a93b73e8de47cb1148b78e0eefbc362478f18768
ramu siripalli <ramsiripalli2772@gmail.com>
2025-05-15 08:45:38 +0530

    Set Basic Routes and added Context API syntax

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Home from './pages/Home.jsx'
+import ApplyJob from './pages/ApplyJob.jsx'
+import Applications from './pages/Applications.jsx'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/apply-job/:id' element={<ApplyJob />} />
+          <Route path='/applications' element={<Applications />} />
+
+      </Routes>
+
+    </div>
   )
 }
 
